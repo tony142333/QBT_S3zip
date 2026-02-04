171e727ca34dfa17d8d145371a422192b768d4a1
tarun <tonystark142333@gmail.com>
2026-02-04 09:45:23 +0530

    update 1

--- TF/variables.tf
+++ TF/variables.tf
@@ -2,4 +2,4 @@ variable "aws_region" { type = string }
 variable "ami_id" { type = string }
 variable "key_name" { type = string }
 variable "s3_bucket_name" { type = string }
-variable "instance_type" { default = "t3.medium" }
+variable "instance_type" { default = "t3.small" }
